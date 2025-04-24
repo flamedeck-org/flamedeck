aed4f3ef108cbed51fe07cfcb4e35585d0aa1c9e
flamedeck-user <support@flamedeck.com>
2025-04-24 14:14:38 -0700

    Working basic folder structure

--- src/pages/Upload.tsx
+++ src/pages/Upload.tsx
@@ -3,13 +3,18 @@ import Layout from "@/components/Layout"; // Use the main Layout
 import PageHeader from "@/components/PageHeader";
 import AuthGuard from "@/components/AuthGuard";
 import { UploadDialog } from "@/components/UploadDialog";
+import { useLocation } from 'react-router-dom'; // <-- Import useLocation
 
 const Upload: React.FC = () => {
+  // Get location state
+  const location = useLocation();
+  const targetFolderId = location.state?.targetFolderId || null;
+
   return (
     <AuthGuard>
       <Layout> 
         <PageHeader title="Upload Trace" />
-        <UploadDialog />
+        <UploadDialog initialFolderId={targetFolderId} />
       </Layout>
     </AuthGuard>
   );
