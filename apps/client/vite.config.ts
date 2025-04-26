d485e4958a0115e66055c49d04d423995df0c521
Zachary Marion <zacharyfmarion@gmail.com>
2025-04-26 15:16:46 -0700

    Set up nx repo

---
+++ apps/client/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
+import { componentTagger } from "lovable-tagger";
+import { viteCommonjs } from '@originjs/vite-plugin-commonjs';
+
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => ({
+  server: {
+    host: "::",
+    port: 8080,
+  },
+  plugins: [
+    react(),
+    viteCommonjs(),
+    mode === 'development' &&
+    componentTagger(),
+  ].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@trace-view-pilot/shared-importer": path.resolve(__dirname, "../../packages/shared-importer/src"),
+    },
+  },
+}));
