a1990d294599889ac78492d2333a6184072e64c4
Zachary Marion <zacharyfmarion@gmail.com>
2025-04-30 08:26:56 -0700

    Working with openai responses API

--- packages/shared-importer/src/index.ts
+++ packages/shared-importer/src/index.ts
@@ -3,5 +3,7 @@ export { exportProfileGroup } from './speedscope-import/file-format.ts';
 // Re-export the type for cleaner imports
 export type { ImporterDependencies } from './speedscope-import/importer-utils.ts';
 
+export { formatPercent } from './speedscope-core/lib-utils.ts';
+
 // Shared profile utils used in both client and server
 export { getDurationMsFromProfileGroup } from './profile-utils.ts';
