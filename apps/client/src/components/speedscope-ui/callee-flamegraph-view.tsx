0e3b836ef0d64b33a25a8a4f925f0a4a19bc51d0
Zachary Marion <zacharyfmarion@gmail.com>
2025-05-01 18:09:10 -0700

    Checkpoint in client screenshotting approach

--- apps/client/src/components/speedscope-ui/callee-flamegraph-view.tsx
+++ apps/client/src/components/speedscope-ui/callee-flamegraph-view.tsx
@@ -5,6 +5,7 @@ import {
   createMemoizedFlamechartRenderer,
   FlamechartViewContainerProps,
   useFlamechartSetters,
+  FlamechartViewHandle,
 } from './flamechart-view-container'
 import {
   getCanvasContext,
@@ -13,7 +14,7 @@ import {
   getFrameToColorBucket,
 } from '../../lib/speedscope-core/app-state/getters'
 import {FlamechartWrapper} from './flamechart-wrapper'
-import React, {memo} from 'react'
+import React, {memo, forwardRef, useRef, useImperativeHandle} from 'react'
 import {useTheme} from './themes/theme'
 import {FlamechartID} from '../../lib/speedscope-core/app-state/profile-group'
 import {flattenRecursionAtom, glCanvasAtom} from '../../lib/speedscope-core/app-state'
@@ -48,12 +49,20 @@ const getCalleeFlamegraph = memoizeByShallowEquality<
 
 const getCalleeFlamegraphRenderer = createMemoizedFlamechartRenderer()
 
-export const CalleeFlamegraphView = memo((ownProps: FlamechartViewContainerProps) => {
-  const {activeProfileState} = ownProps
+export const CalleeFlamegraphView = memo(forwardRef<FlamechartViewHandle, FlamechartViewContainerProps>((props, ref) => {
+  const {activeProfileState} = props
   const {profile, sandwichViewState} = activeProfileState
   const flattenRecursion = useAtom(flattenRecursionAtom)
   const glCanvas = useAtom(glCanvasAtom)
   const theme = useTheme()
+
+  const flamechartWrapperRef = useRef<FlamechartViewHandle>(null);
+
+  useImperativeHandle(ref, () => ({
+    drawOverlayOnto: (targetCtx: CanvasRenderingContext2D) => {
+      flamechartWrapperRef.current?.drawOverlayOnto(targetCtx);
+    }
+  }));
 
   if (!profile) throw new Error('profile missing')
   if (!glCanvas) throw new Error('glCanvas missing')
@@ -74,6 +83,7 @@ export const CalleeFlamegraphView = memo((ownProps: FlamechartViewContainerProps
 
   return (
     <FlamechartWrapper
+      ref={flamechartWrapperRef}
       theme={theme}
       renderInverted={false}
       flamechart={flamechart}
@@ -82,8 +92,7 @@ export const CalleeFlamegraphView = memo((ownProps: FlamechartViewContainerProps
       getCSSColorForFrame={getCSSColorForFrame}
       {...useFlamechartSetters(FlamechartID.SANDWICH_CALLEES)}
       {...callerCallee.calleeFlamegraph}
-      // This overrides the setSelectedNode specified in useFlamechartSettesr
       setSelectedNode={noop}
     />
   )
-})
+}));
