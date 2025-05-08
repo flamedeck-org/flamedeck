393feef0ba820966b7afd63ff247e3d7cd151447
Zachary Marion <zacharyfmarion@gmail.com>
2025-05-08 09:37:29 -0700

    Set up eslint

---
+++ .prettierrc.cjs
@@ -0,0 +1,14 @@
+module.exports = {
+  printWidth: 100, // Try to keep lines under 100 characters
+  tabWidth: 2, // Number of spaces per indentation-level.
+  useTabs: false, // Indent lines with spaces instead of tabs.
+  semi: true, // Print semicolons at the ends of statements.
+  singleQuote: false, // Use double quotes for strings.
+  quoteProps: "as-needed", // Only add quotes around object properties where required.
+  jsxSingleQuote: false, // Use double quotes in JSX.
+  trailingComma: "es5", // Trailing commas where valid in ES5 (objects, arrays, etc.)
+  bracketSpacing: true, // Print spaces between brackets in object literals.
+  bracketSameLine: false, // Put the > of a multi-line HTML (HTML, JSX, Vue, Angular) element at the end of the last line instead of being alone on the next line.
+  arrowParens: "always", // Always include parens around a sole arrow function parameter.
+  endOfLine: "lf", // Line ending character
+}; 
